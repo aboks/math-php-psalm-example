f685e2c375dfb2b4fba08ae3d364720bbc55706b
Arnout Boks <arnoutboks@gmail.com>
2023-05-09 09:58:31 +0200

    Add some more examples of Psalm-incompatible types

--- example.php
+++ example.php
@@ -1,5 +1,13 @@
 <?php
 use MathPHP\LinearAlgebra\NumericMatrix;
+use MathPHP\LinearAlgebra\NumericSquareMatrix;
+use MathPHP\LinearAlgebra\Vector;
 
 $A = new NumericMatrix([[1, 2, 3], [4, 5, 6]]);
 print_r($A);
+
+$B = new NumericSquareMatrix([[1, 2], [3, 4]]);
+print_r($B);
+
+$v = new Vector([1, 2]);
+print_r($v);
